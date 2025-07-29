c907b22d5e09cbfbdc632491802456fcabd01852
S0n1c <itss0n1cdev@gmail.com>
2025-07-28 21:13:19 -0400

    Initial Commit

---
+++ habi/habiApp.swift
@@ -0,0 +1,17 @@
+//
+//  habiApp.swift
+//  habi
+//
+//  Created by war on 2025-07-28.
+//
+
+import SwiftUI
+
+@main
+struct habiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
